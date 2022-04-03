ccdabb627050801b7e9e9f83043df3fbad2451d8
unknown <sdvelu18@gmail.com>
2022-04-03 02:10:29 -0400

    added style changes to dashboard

--- receipts/src/components/HomePage.js
+++ receipts/src/components/HomePage.js
@@ -5,23 +5,32 @@ function HomePage() {
   return (
     <div class="home-page">
         <h1 id="website-title">Receipted</h1>
-        <form>
-            <label> Store Brand:
-                <select name="stores" id="stores">
-                    <option value="shoprite">ShopRite</option>
-                    <option value="costco">Costco</option>
-                    <option value="stop-and-shop">Stop & Shop</option>
-                    <option value="aldi">Aldi</option>
-                </select>
-            </label>
-            <br></br>   
-            <label> Receipt Code:
-                <input type="text" name="name" />
-            </label>
-            <br></br>
-            <input type="submit" value="Go!" />
-        </form>
         <ReceiptParticles />
+        <div class="login-card">
+            <div class="login-card-content">
+                <form class="form-field">
+                    <label>Store Brand: </label>
+                        <select name="stores" id="stores">
+                            <option value="shoprite">ShopRite</option>
+                            <option value="costco">Costco</option>
+                            <option value="stop-and-shop">Stop & Shop</option>
+                            <option value="aldi">Aldi</option>
+                        </select>   
+                    <div class="form-field">
+                        <label> Receipt Code: 
+                            <input type="text" name="name" />
+                        </label>
+                    </div>
+                </form>
+                <div>
+                <br></br>
+                </div>
+                <button id="submit" type="submit">
+                    Go
+                </button>
+                
+            </div>
+        </div>
     </div>
   );
 }
